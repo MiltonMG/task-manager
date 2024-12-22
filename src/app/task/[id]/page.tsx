a4a50c12a2029acc2073e22c797f90ac4b86614b
Milton MG <milton.elsalvador26@gmail.com>
2024-12-21 18:23:07 -0600

    How to get params from the URL in a page component comment

--- src/app/task/[id]/page.tsx
+++ src/app/task/[id]/page.tsx
@@ -2,6 +2,9 @@ import { TaskForm } from "@/app/new/task-form";
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
 
+// How to get params from the URL in a page component
+// Code example here
+//Edit({ params }: { params: { id: string } })
 export default async function Edit({ params }: { params: { id: string } }) {
     const id = parseInt(await params.id);
     
